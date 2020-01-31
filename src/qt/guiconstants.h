632c2a062f1b3137a8d38ffe2eeab9d5886a0e40
Peter Bushnell <bushsolo@gmail.com>
2020-01-31 06:59:52 +0000

    Change TLD

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -47,7 +47,7 @@ static const int MAX_URI_LENGTH = 255;
 #define SPINNER_FRAMES 36
 
 #define QAPP_ORG_NAME "Mooncoin"
-#define QAPP_ORG_DOMAIN "mooncoin.eco"
+#define QAPP_ORG_DOMAIN "mooncoin.com"
 #define QAPP_APP_NAME_DEFAULT "Mooncoin-Qt"
 #define QAPP_APP_NAME_TESTNET "Mooncoin-Qt-testnet"
 #define QAPP_APP_NAME_REGTEST "Mooncoin-Qt-regtest"
--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"eco.mooncoin.Mooncoin-Qt";
+        return @"com.mooncoin.Mooncoin-Qt";
     } else {
         return [self __bundleIdentifier];
     }
