9d7af8a1667bebbca2ee84386bd912807ef04e97
Peter Bushnell <bushsolo@gmail.com>
2020-02-22 08:37:48 +0000

    Update seeder and fixed seeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -119,9 +119,7 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("node0.bazco.in");
-        vSeeds.emplace_back("node1.bazco.in");
-        vSeeds.emplace_back("bazco.in");
+        vSeeds.emplace_back("moon.alltheco.in");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,3);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,22);
